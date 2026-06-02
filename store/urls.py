0c3fe384a51eef5256e91b867c13d755761d840f
Jeevan Neupane <jeevannar16@gmail.com>
2026-06-02 18:26:12 +0545

    Commit all function now add to cart and whitelist remained

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+   
   
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart'),
     path('checkout/', views.checkout_view, name='checkout'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+
+    
+path('sale/', views.sale_catalog, name='sale_catalog'),
 ]
+
+

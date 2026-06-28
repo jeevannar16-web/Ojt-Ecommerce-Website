8c32f1c661751b794160ced688777b116365fd2d
Jeevan Neupane <jeevannar16@gmail.com>
2026-06-28 16:12:30 +0545

    Replace insecure default SECRET_KEY with generated key

--- fitness_hub/settings.py
+++ fitness_hub/settings.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'REPLACED_PLACEHOLDER_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'REPLACED_SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
